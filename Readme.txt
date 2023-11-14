d11cfc9882a9d75c43f017c843014c7167a988fb
olawanlejoeldanielafriheart <esuoladaniel002@gmail.com>
2023-11-14 19:06:27 +0100

    Optimising stuff

--- Readme.txt
+++ Readme.txt
@@ -1,6 +1,2 @@
-Thanks for downloading this template!
-
 Template Name: Mayor-K Prime Properties
-Template URL: https://bootstrapmade.com/Mayor-K Prime Properties-bootstrap-construction-website-template/
-Author: BootstrapMade.com
-License: https://bootstrapmade.com/license/
+Author: Esuola Daniel
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,3 @@
-/**
-* Template Name: Mayor-K Prime Properties
-* Updated: Sep 18 2023 with Bootstrap v5.3.2
-* Template URL: https://bootstrapmade.com/Mayor-K Prime Properties-bootstrap-construction-website-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
 document.addEventListener('DOMContentLoaded', () => {
   "use strict";
 
